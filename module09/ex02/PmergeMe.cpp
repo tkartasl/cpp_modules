dcbc9695f5e1a6d862e021f91462158d9931558e
tkartasl <tomi.kartaslammi@gmail.com>
2024-12-05 15:39:13 +0200

    Added merge sort algorithm

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -16,18 +16,22 @@ PmergeMe& PmergeMe::operator=(PmergeMe const& src) {
 
 void PmergeMe::_validateNumber(std::string& arg) {
     if (arg.length() < 1)
-        throw std::exception;
+        throw std::exception();
     for (char ch : arg) {
         if (std::isdigit(ch) == 0)
-            throw std::exception;
+            throw std::exception();
     }
     int number = stoi(arg);
-    _numbers.push_back(number);
+    for (int i : _numbers) {
+		if (i == number)
+			throw std::exception();
+	}
+	_numbers.push_back(number);
 }
 
 bool PmergeMe::_validateInput() {
-    size_t startPos = 0;
-    size_t endPos = 0;
+    int startPos = 0;
+    int endPos = 0;
     std::string arg;
 
     try {
@@ -45,10 +49,99 @@ bool PmergeMe::_validateInput() {
     return true;
 }
 
+void PmergeMe::_printBefore() {
+	std::cout << "Before: ";
+	for (int i : _numbers) {
+		std::cout << i;
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::_makePairs() {
+	t_pair pair;
+	int sizeOfVector = _numbers.size();
+
+	for (int i = 0; i < sizeOfVector; i++) {
+		if (i + 1 < sizeOfVector) {
+			if (_numbers[i] < _numbers[i + 1]) {
+				pair.small = _numbers[i];
+				pair.big = _numbers[i + 1];
+			}
+			else {
+				pair.small = _numbers[i + 1];
+				pair.big = _numbers[i];
+			}
+			_mainChain.push_back(pair.big);
+		}
+		else {
+			pair.small = _numbers[i];
+			pair.big = -1;
+		}
+		_numberPairs.push_back(pair);
+	}
+}
+
+void PmergeMe::_merge(int start, int middle, int end) {
+	std::vector<int> left;
+	std::vector<int> right;
+	int sizeLeft = middle - start + 1;
+	int sizeRight = end - middle;
+	
+	for (int i = 0; i < sizeLeft; i++) {
+		left[i] = _mainChain[start + i];
+	}
+	for (int j = 0; j < sizeRight; j++) {
+		right[j] = right[middle + 1 + j];
+	}
+
+	int idxLeft = 0;
+	int idxRight = 0;
+	int i = start;
+
+	while (idxLeft < sizeLeft && idxRight < sizeRight) {
+		if (left[idxLeft] < right[idxRight]) {
+			_mainChain[i] = left[idxLeft];
+			idxLeft++;
+		}
+		else {
+			_mainChain[i] = right[idxRight];
+			idxRight++;
+		}
+		i++;
+	}
+
+	while (idxLeft < sizeLeft) {
+		_mainChain[i] = left[idxLeft];
+		i++;
+		idxLeft++;
+	}
+	while (idxRight < sizeRight) {
+		_mainChain[i] = right[idxRight];
+		i++;
+		idxRight++;
+	}
+}
+
+void PmergeMe::_mergeSort(int start, int end) {
+	if (start > end)
+		return;
+	int middle = (start + end) / 2;
+	_mergeSort(start, middle);
+	_mergeSort(middle + 1, end);
+	_merge(start, middle, end);
+}
+
+void PmergeMe::_insertionSort() {
+	
+}
+
 void PmergeMe::sort() {
     if (!_validateInput()) {
         std::cerr << "Error: invalid input" << std::endl;
         return ;
     }
-    
+	_printBefore();
+    _makePairs();
+	_mergeSort(0, _mainChain.size() - 1);
+	_insertionSort();
 }
--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -3,6 +3,11 @@
 # include <iostream>
 # include <vector>
 
+typedef struct s_pair {
+	int small;
+	int big;
+} t_pair;
+
 class PmergeMe
 {
     public:
@@ -16,7 +21,14 @@ class PmergeMe
         std::string _input;
         bool _validateInput();
         void _validateNumber(std::string&);
+		void _printBefore();
+		void _makePairs();
+		void _mergeSort(int, int);
+		void _insertionSort();
+		void _merge(int, int, int);
         std::vector<int> _numbers;
+		std::vector<t_pair> _numberPairs;
+		std::vector<int> _mainChain;
 };
 
 #endif
